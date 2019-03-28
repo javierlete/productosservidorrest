9e563d3750dbdca9d464882ef6d1cfdfcb17c46e
Javier Lete <jglete@gmail.com>
2019-03-28 20:32:52 +0100

    Creación de la WEB API con CORS permisivo

---
+++ ProductosRestApi/Models/Producto.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ProductosRestApi.Models
+{
+    [Table("Productos")]
+    public class Producto
+    {
+        [JsonProperty(PropertyName = "id")]
+        public int Id { get; set; }
+        [JsonProperty(PropertyName = "nombre")]
+        public string Nombre { get; set; }
+        [JsonProperty(PropertyName = "descripcion")]
+        public string Descripcion { get; set; }
+        [JsonProperty(PropertyName = "precio")]
+        public decimal Precio { get; set; }
+    }
+}
---
+++ ProductosRestApi/Models/ProductosRestApiContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ProductosRestApi.Models
+{
+    public class ProductosRestApiContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public ProductosRestApiContext() : base("name=ProductosRestApiContext")
+        {
+            Database.SetInitializer<ProductosRestApiContext>(new DropCreateDatabaseAlways<ProductosRestApiContext>());
+        }
+
+        public System.Data.Entity.DbSet<ProductosRestApi.Models.Producto> Productos { get; set; }
+    }
+}
